7c60ae62d864eab070246b945ce02f184a3049d6
Arto <artsargsyan93@gmail.com>
2020-05-27 05:58:17 +0400

    Revert "  Added Second"
    This reverts commit ef46d3954a6c5b0dd26c5b43e9c96768e6945d3c.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -4,10 +4,6 @@ void My_Foo() {
 	std::cout << "Second commit";
 }
 
-voit My_Foo() {
-	std::cout << "Second Commit" << std::endl
-}
-
 int main() {
 	std::cout << "Hello World!" << std::endl;
 	return 0;
